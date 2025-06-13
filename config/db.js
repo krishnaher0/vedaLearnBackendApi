d80ad1ff482283455a5c9217855284c82bc77f61
krishnaher0 <krishna613460@gmail.com>
2025-06-13 21:21:47 +0545

    add mongodb database in the backend server

--- config/db.js
+++ config/db.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 exports.connectDB = async () => {
+    //connect mongobd database (remote) in the backend server for playing with data
     try {
         await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
