3dfc602c252f78285e645dab626296cd637e550a
krishnaher0 <krishna613460@gmail.com>
2025-06-13 21:19:45 +0545

    backend api routes for registering and login user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,7 @@ const { registerUser,loginUser } = require("../controllers/userController");
 const router = express.Router();
  
  
-router.post("/register",registerUser)
-router.post("/login",loginUser)
+router.post("/register",registerUser)//navigate to the register api
+router.post("/login",loginUser) //navigate to login 
  
 module.exports = router;
