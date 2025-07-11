021286bfb79556fc6a83a419af8808eda10de2d6
krishnaher0 <krishna613460@gmail.com>
2025-07-11 23:56:11 +0545

    Enhance file upload middleware: allow audio file uploads and increase file size limit to 50MB

--- middlewares/fileUpload.js
+++ middlewares/fileUpload.js
@@ -14,15 +14,15 @@ const storage = multer.diskStorage({
 });
 
 const fileFilter = (req, file, cb) => {
-  if (file.mimetype.startsWith("image")) {
+  if (file.mimetype.startsWith("image")|| file.mimetype.startsWith("audio")) {
     cb(null, true);
   } else {
-    cb(new Error("Only image files are allowed!"), false);
+    cb(new Error("Only image  and audio files are allowed!"), false);
   }
 };
 const upload = multer({
     storage,
-    limits: { fileSize: 5 * 1024 * 1024 }, // 5 mb optional
+    limits: { fileSize: 50 * 1024 * 1024 }, // 5 mb optional
     fileFilter // optional
 })
 
