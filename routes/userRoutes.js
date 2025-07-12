f1b08f6eb29689ba02da17da70e3b80d94de131c
krishnaher0 <krishna613460@gmail.com>
2025-07-12 23:45:13 +0545

    Format user routes: add spacing for improved readability

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const { registerUser,loginUser } = require("../controllers/userController");
 const router = express.Router();
+
  
  
 router.post("/register",registerUser)//navigate to the register api
 router.post("/login",loginUser) //navigate to login 
+
+
  
 module.exports = router;
