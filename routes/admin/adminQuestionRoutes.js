20db19f990ff16f67b159d3e7d7cfc48d37ff553
krishnaher0 <krishna613460@gmail.com>
2025-07-12 23:45:25 +0545

    Refactor admin question routes: integrate file upload middleware for question creation and updates

--- routes/admin/adminQuestionRoutes.js
+++ routes/admin/adminQuestionRoutes.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const upload = require("../../middlewares/fileUpload")
 const controller = require('../../controllers/admin/questionManagement');
 
-router.post('/', controller.createQuestion);
+router.post('/', upload.single('audioUrl'), controller.createQuestion);
 router.get('/', controller.getQuestions);
 router.get('/:id', controller.getQuestionById);
-router.put('/:id', controller.updateQuestion);
+router.get('/lesson/:lessonId', controller.getQuestionsByLesson);
+router.put('/:id', upload.single('audioUrl'), controller.updateQuestion);
+router.delete("/many",controller.deleteAllQuestions)
 router.delete('/:id', controller.deleteQuestion);
+router.post("/many",controller.addManyQuestions)
+
+// router.post("/all",controller.createQuestionsBatch)
+
 
 module.exports = router;
