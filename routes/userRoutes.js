8a84c308a3f125c2ce4c22886a031777c8bc0c41
krishnaher0 <krishna613460@gmail.com>
2025-07-15 18:43:39 +0545

    routes for forgot password

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,13 @@
 const express = require("express");
-const { registerUser,loginUser } = require("../controllers/userController");
+const { registerUser,loginUser ,sendResetLink,resetPassword} = require("../controllers/userController");
 const router = express.Router();
 
  
  
 router.post("/register",registerUser)//navigate to the register api
 router.post("/login",loginUser) //navigate to login 
+router.post("/request-reset", sendResetLink);
+router.post("/reset-password/:token", resetPassword);
 
 
  
