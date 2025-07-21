c64ff1012c817f944e3cbb60d6454c4ef653c196
krishnaher0 <krishna613460@gmail.com>
2025-07-21 23:35:57 +0545

    Update plan routes for CRUD plans

--- routes/planRoutes.js
+++ routes/planRoutes.js
@@ -1,15 +1,11 @@
 const express = require("express");
-const PlanController = require("../controllers/planController");
-
 const router = express.Router();
+const planController = require("../controllers/planController");
 
-// Public routes
-router.get("/", PlanController.getAll);
-router.get("/:id", PlanController.getOne);
-
-// Admin-only routes (protect with middleware if needed)
-router.post("/", PlanController.create);
-router.put("/:id", PlanController.update);
-router.delete("/:id", PlanController.delete);
+router.post("/", planController.createPlan);
+router.get("/", planController.getAllPlans);
+router.get("/:id", planController.getPlanById);
+router.put("/:id", planController.updatePlan);
+router.delete("/:id", planController.deletePlan);
 
 module.exports = router;
